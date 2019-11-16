756f4cecda3745f38351529c0757207173d6f105
Hugo SOHM <hugo.sohm@epitech.eu>
2019-11-16 01:39:22 +0100

    [+] Added Weather, Twitch and Youtube services with API

--- server/src/main/java/com/dashboard/server/ServerApplication.java
+++ server/src/main/java/com/dashboard/server/ServerApplication.java
@@ -1,5 +1,6 @@
 package com.dashboard.server;
 
+import com.dashboard.server.services.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,9 @@ import java.sql.DriverManager;
 public class ServerApplication {
 
 	public static void main(String args[]) {
+		YoutubeService YoutubeService = new YoutubeService();
+		OpenWeatherService OpenWeatherService = new OpenWeatherService();
+		TwitchService TwitchService = new TwitchService();
 		SpringApplication.run(ServerApplication.class, args);
 	}
 }
