cabf55c7c332725736446050bfecbdee38947eb7
Hugo SOHM <hugo.sohm@epitech.eu>
2019-11-17 22:36:05 +0100

    [+] Added modal close after submit and modals js file

---
+++ server/src/main/resources/static/assets/js/dashboard-modals.js
@@ -0,0 +1,11 @@
+function hideYoutubeModal() {
+    $('#YoutubeModal').modal('hide');
+}
+
+function hideTwitchModal() {
+    $('#YoutubeModal').modal('hide');
+}
+
+function hideWeatherModal() {
+    $('#YoutubeModal').modal('hide');
+}
